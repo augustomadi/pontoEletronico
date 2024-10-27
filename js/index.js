b3ea249fcec66f5a39d562a849fd8a181a25eecb
joao-guilherme-costa <bilejg@gmail.com>
2024-10-27 19:24:24 -0300

    resolving some errors

--- js/index.js
+++ js/index.js
@@ -9,16 +9,16 @@ const dialogPonto = document.getElementById("dialog-ponto");
 
 const btnDialogFechar = document.getElementById("btn-dialog-fechar");
 btnDialogFechar.addEventListener("click", () => {
-    dialogPonto.close();
+  dialogPonto.close();
 });
 
 const nextRegister = {
-    "entrada": "intervalo",
-    "intervalo": "volta-intervalo", 
-    "volta-intervalo": "saida", 
-    "saida": "entrada",
-    "falta": "entrada"
-}
+  entrada: "intervalo",
+  intervalo: "volta-intervalo",
+  "volta-intervalo": "saida",
+  saida: "entrada",
+  falta: "entrada",
+};
 
 let registerLocalStorage = getRegisterLocalStorage();
 
@@ -27,217 +27,249 @@ const dialogHora = document.getElementById("dialog-hora");
 
 const divAlertaRegistroPonto = document.getElementById("alerta-registro-ponto");
 
-// Função para fechar o alerta
-const btnCloseAlertRegister = document.getElementById("alerta-registro-ponto-fechar");
+const btnCloseAlertRegister = document.getElementById(
+  "alerta-registro-ponto-fechar"
+);
 btnCloseAlertRegister.addEventListener("click", () => {
-    divAlertaRegistroPonto.classList.remove("show");
-    divAlertaRegistroPonto.classList.add("hidden");
+  divAlertaRegistroPonto.classList.remove("show");
+  divAlertaRegistroPonto.classList.add("hidden");
 });
 
 diaSemana.textContent = getWeekDay();
 diaMesAno.textContent = getCurrentDate();
 
 async function getCurrentPosition() {
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            let userLocation = {
-                "latitude": position.coords.latitude,
-                "longitude": position.coords.longitude
-            }
-            resolve(userLocation);
-        },
-        (error) => {
-            reject("Erro ao recuperar a localização " + error);
-        });
-    });
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        let userLocation = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        };
+        resolve(userLocation);
+      },
+      (error) => {
+        reject("Erro ao recuperar a localização " + error);
+      }
+    );
+  });
 }
 
 function getNextId() {
-    let lastId = localStorage.getItem("lastId");
+  let lastId = localStorage.getItem("lastId");
 
-    if (!lastId) {
-        lastId = 0; 
-    }
+  if (!lastId) {
+    lastId = 0;
+  }
 
-    const nextId = parseInt(lastId) + 1;
+  const nextId = parseInt(lastId) + 1;
 
-    localStorage.setItem("lastId", nextId);
+  localStorage.setItem("lastId", nextId);
 
-    return nextId;
+  return nextId;
 }
 
 const btnDialogBaterPonto = document.getElementById("btn-dialog-bater-ponto");
 const selectTipoPonto = document.getElementById("tipos-ponto");
 const divComentarioPonto = document.getElementById("div-comentario-opcional");
-const comentarioPontoTextarea = document.getElementById("comentario-opcional-textarea");
+const comentarioPontoTextarea = document.getElementById(
+  "comentario-opcional-textarea"
+);
 const divAnexoImagem = document.getElementById("div-anexo-imagem");
-const divJustificativaPonto = document.getElementById("div-justificativa-ponto");
-const justificativaPontoTextarea = document.getElementById("justificativa-ponto-textarea");
+const divJustificativaPonto = document.getElementById(
+  "div-justificativa-ponto"
+);
+const justificativaPontoTextarea = document.getElementById(
+  "justificativa-ponto-textarea"
+);
 
-// Função para mostrar/ocultar os campos
-selectTipoPonto.addEventListener("change", function() {
-    const typeRegister = selectTipoPonto.value;
+selectTipoPonto.addEventListener("change", function () {
+  const typeRegister = selectTipoPonto.value;
 
-    // Sempre mostra o campo de comentário
-    divComentarioPonto.style.display = "block";
+  divComentarioPonto.style.display = "block";
 
-    // Mostra o campo de justificativa apenas para "falta"
-    if (typeRegister === "falta") {
-        divJustificativaPonto.style.display = "block"; // Mostra o campo de justificativa
-    } else {
-        divJustificativaPonto.style.display = "none"; // Esconde o campo de justificativa
-        justificativaPontoTextarea.value = "";  // Limpa o campo de justificativa se não for "falta"
-    }
+  if (typeRegister === "falta") {
+    divJustificativaPonto.style.display = "block";
+  } else {
+    divJustificativaPonto.style.display = "none";
+    justificativaPontoTextarea.value = "";
+  }
 
-    // Mostra o campo de imagem para todos os tipos
-    divAnexoImagem.style.display = "block"; 
+  divAnexoImagem.style.display = "block";
 });
 
 function toBase64(file) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = (error) => reject(error);
-    });
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
 }
 
 btnDialogBaterPonto.addEventListener("click", async () => {
-    const dataPonto = document.getElementById("data-ponto").value;
-    const typeRegister = selectTipoPonto.value;
+  const dataPonto = document.getElementById("data-ponto").value;
+  const typeRegister = selectTipoPonto.value;
 
-    if (!dataPonto) {
-        alert("Por favor, escolha uma data e hora antes de registrar o ponto.");
-        return;
-    }
+  if (!dataPonto) {
+    alert("Por favor, escolha uma data e hora antes de registrar o ponto.");
+    return;
+  }
 
-    const justificativa = (typeRegister === "falta") ? justificativaPontoTextarea.value.trim() : null;
-    const comentarioOpcional = comentarioPontoTextarea.value.trim();
-    const anexoImagemInput = document.getElementById("anexo-imagem");
-    const anexoImagem = anexoImagemInput ? anexoImagemInput.files[0] : null;
+  const justificativa =
+    typeRegister === "falta" ? justificativaPontoTextarea.value.trim() : null;
+  const comentarioOpcional = comentarioPontoTextarea.value.trim();
+  const anexoImagemInput = document.getElementById("anexo-imagem");
+  const anexoImagem = anexoImagemInput ? anexoImagemInput.files[0] : null;
 
-    let base64Image = null;
-    if (anexoImagem) {
-        base64Image = await toBase64(anexoImagem);
-    }
+  let base64Image = null;
+  if (anexoImagem) {
+    base64Image = await toBase64(anexoImagem);
+  }
 
-    const selectedDate = new Date(dataPonto);
-    const currentDate = new Date();
+  const selectedDate = new Date(dataPonto);
+  const currentDate = new Date();
 
-    if (selectedDate > currentDate) {
-        alert("Você não pode registrar pontos em datas futuras.");
-        return;
-    }
+  if (selectedDate > currentDate) {
+    alert("Você não pode registrar pontos em datas futuras.");
+    return;
+  }
 
-    if (typeRegister === "falta" && !justificativa) {
-        alert("Em caso de falta, escreva sua justificativa.");
-        return;
-    }
+  if (typeRegister === "falta" && !justificativa) {
+    alert("Em caso de falta, escreva sua justificativa.");
+    return;
+  }
 
-    const dataPassada = selectedDate < currentDate.setHours(0, 0, 0, 0);
+  const dataPassada = selectedDate < currentDate.setHours(0, 0, 0, 0);
 
-    let userCurrentPosition = await getCurrentPosition();
+  let userCurrentPosition = await getCurrentPosition();
 
-    let pontoId = getNextId();
+  let pontoId = getNextId();
 
-    let ponto = {
-        "id": pontoId, 
-        "data": selectedDate.toLocaleDateString(),
-        "hora": selectedDate.toLocaleTimeString(),
-        "localizacao": userCurrentPosition,
-        "tipo": typeRegister,
-        "justificativa": justificativa ? justificativa : null,
-        "comentario_opcional": comentarioOpcional ? comentarioOpcional : null,
-        "imagem": base64Image,
-        "data_passada": dataPassada
-    };
+  let ponto = {
+    id: pontoId,
+    data: selectedDate.toLocaleDateString(),
+    hora: selectedDate.toLocaleTimeString(),
+    localizacao: userCurrentPosition,
+    tipo: typeRegister,
+    justificativa: justificativa ? justificativa : null,
+    comentario_opcional: comentarioOpcional ? comentarioOpcional : null,
+    imagem: base64Image,
+    data_passada: dataPassada,
+  };
 
-    saveRegisterLocalStorage(ponto);
+  saveRegisterLocalStorage(ponto);
 
-    localStorage.setItem("lastDateRegister", ponto.data);
-    localStorage.setItem("lastTimeRegister", ponto.hora);
+  localStorage.setItem("lastDateRegister", ponto.data);
+  localStorage.setItem("lastTimeRegister", ponto.hora);
 
-    dialogPonto.close();
+  dialogPonto.close();
 
-    // Exibir o alerta após o registro
-    divAlertaRegistroPonto.classList.remove("hidden");
-    divAlertaRegistroPonto.classList.add("show");
+  divAlertaRegistroPonto.classList.remove("hidden");
+  divAlertaRegistroPonto.classList.add("show");
 
-    setTimeout(() => {
-        divAlertaRegistroPonto.classList.remove("show");
-        divAlertaRegistroPonto.classList.add("hidden");
-    }, 5000);
+  setTimeout(() => {
+    divAlertaRegistroPonto.classList.remove("show");
+    divAlertaRegistroPonto.classList.add("hidden");
+  }, 5000);
 });
 
 function saveRegisterLocalStorage(register) {
-    registerLocalStorage.push(register);
-    localStorage.setItem("register", JSON.stringify(registerLocalStorage));
-    localStorage.setItem("lastTypeRegister", register.tipo);
-} 
+  registerLocalStorage.push(register);
+  localStorage.setItem("register", JSON.stringify(registerLocalStorage));
+  localStorage.setItem("lastTypeRegister", register.tipo);
+}
 
 function getRegisterLocalStorage() {
-    let registers = localStorage.getItem("register");
+  let registers = localStorage.getItem("register");
 
-    if (!registers) {
-        return [];
-    }
+  if (!registers) {
+    return [];
+  }
 
-    return JSON.parse(registers); 
+  return JSON.parse(registers);
 }
 
 function register() {
-    dialogData.textContent = "Data: " + getCurrentDate();
+  dialogData.textContent = "Data: " + getCurrentDate();
+  dialogHora.textContent = "Hora: " + getCurrentHour();
+
+  let lastTypeRegister = localStorage.getItem("lastTypeRegister");
+  if (lastTypeRegister) {
+    const typeRegister = document.getElementById("tipos-ponto");
+    typeRegister.value = nextRegister[lastTypeRegister];
+    let lastRegisterText =
+      "Último registro: " +
+      localStorage.getItem("lastDateRegister") +
+      " - " +
+      localStorage.getItem("lastTimeRegister") +
+      " | " +
+      localStorage.getItem("lastTypeRegister");
+    document.getElementById("dialog-last-register").textContent =
+      lastRegisterText;
+  }
+
+  setInterval(() => {
     dialogHora.textContent = "Hora: " + getCurrentHour();
-    
-    let lastTypeRegister = localStorage.getItem("lastTypeRegister");
-    if (lastTypeRegister) {
-        const typeRegister = document.getElementById("tipos-ponto");
-        typeRegister.value = nextRegister[lastTypeRegister];
-        let lastRegisterText = "Último registro: " + localStorage.getItem("lastDateRegister") + " - " + localStorage.getItem("lastTimeRegister") + " | " + localStorage.getItem("lastTypeRegister");
-        document.getElementById("dialog-last-register").textContent = lastRegisterText;
-    }
+  }, 1000);
 
-    setInterval(() => {
-        dialogHora.textContent = "Hora: " + getCurrentHour();
-    }, 1000);
-
-    dialogPonto.showModal();
+  dialogPonto.showModal();
 }
 
 function getWeekDay() {
-    const date = new Date();
-    let days = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
-    return days[date.getDay()];
+  const date = new Date();
+  let days = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+  ];
+  return days[date.getDay()];
 }
 
 function getCurrentHour() {
-    const date = new Date();
-    return String(date.getHours()).padStart(2, '0') + ":" + String(date.getMinutes()).padStart(2, '0') + ":" + String(date.getSeconds()).padStart(2, '0');
+  const date = new Date();
+  return (
+    String(date.getHours()).padStart(2, "0") +
+    ":" +
+    String(date.getMinutes()).padStart(2, "0") +
+    ":" +
+    String(date.getSeconds()).padStart(2, "0")
+  );
 }
 
 function getCurrentDate() {
-    const date = new Date();
-    return String(date.getDate()).padStart(2, '0') + "/" + String((date.getMonth() + 1)).padStart(2, '0') + "/" + String(date.getFullYear()).padStart(2, '0');
+  const date = new Date();
+  return (
+    String(date.getDate()).padStart(2, "0") +
+    "/" +
+    String(date.getMonth() + 1).padStart(2, "0") +
+    "/" +
+    String(date.getFullYear()).padStart(2, "0")
+  );
 }
 
 function printCurrentHour() {
-    horaMinSeg.textContent = getCurrentHour();
+  horaMinSeg.textContent = getCurrentHour();
 }
 
 const btnDataHoraAtual = document.getElementById("btn-data-hora-atual");
 btnDataHoraAtual.addEventListener("click", () => {
-    const dataPonto = document.getElementById("data-ponto");
-    const now = new Date();
-    
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0'); 
-    const day = String(now.getDate()).padStart(2, '0');
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
+  const dataPonto = document.getElementById("data-ponto");
+  const now = new Date();
 
-    const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}`;
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
 
-    dataPonto.value = formattedDate;
+  const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}`;
+
+  dataPonto.value = formattedDate;
 });
 
 printCurrentHour();
--- js/relatorio.js
+++ js/relatorio.js
@@ -1,193 +1,189 @@
 // Função para renderizar a lista de registros
 function renderList(registros) {
-    const tabelaBody = document.querySelector("#tabela-registros tbody");
-    tabelaBody.innerHTML = ''; // Limpa a tabela antes de renderizar
+  const tabelaBody = document.querySelector("#tabela-registros tbody");
+  tabelaBody.innerHTML = "";
 
-    registros.forEach((registro) => {
-        const row = document.createElement("tr");
+  registros.forEach((registro) => {
+    const row = document.createElement("tr");
 
-        const imagemCell = registro.imagem 
-            ? `<img src="${registro.imagem}" alt="Imagem" style="width: 100px; height: auto;">`
-            : "-";
+    const imagemCell = registro.imagem
+      ? `<img src="${registro.imagem}" alt="Imagem" style="width: 100px; height: auto;">`
+      : "-";
 
-        const dataPassadaText = registro.data_passada ? 'Sim' : 'Não';
+    const dataPassadaText = registro.data_passada ? "Sim" : "Não";
 
-        // Botões de atualização ao lado de cada campo
-        const botaoAtualizarComentario = `<button class="btn-atualizar-comentario" data-id="${registro.id}">Atualizar</button>`;
-        const botaoAtualizarTipo = `<button class="btn-atualizar-tipo" data-id="${registro.id}">Atualizar</button>`;
-        const botaoAtualizarData = `<button class="btn-atualizar-data" data-id="${registro.id}">Atualizar</button>`;
-        const botaoAtualizarHora = `<button class="btn-atualizar-hora" data-id="${registro.id}">Atualizar</button>`;
-        const botaoApagar = `<button class="btn-apagar" data-id="${registro.id}">Apagar</button>`; // Botão de apagar
+    const botaoAtualizarComentario = `<button class="btn-atualizar-comentario" data-id="${registro.id}">Atualizar</button>`;
+    const botaoAtualizarTipo = `<button class="btn-atualizar-tipo" data-id="${registro.id}">Atualizar</button>`;
+    const botaoAtualizarData = `<button class="btn-atualizar-data" data-id="${registro.id}">Atualizar</button>`;
+    const botaoAtualizarHora = `<button class="btn-atualizar-hora" data-id="${registro.id}">Atualizar</button>`;
+    const botaoApagar = `<button class="btn-apagar" data-id="${registro.id}">Apagar</button>`;
 
-        row.innerHTML = `
+    row.innerHTML = `
             <td>${registro.id}</td>
             <td>${registro.data} <br> ${botaoAtualizarData}</td>
             <td>${registro.hora} <br> ${botaoAtualizarHora}</td>
             <td>${registro.tipo} <br> ${botaoAtualizarTipo}</td>
             <td>${registro.justificativa || "-"}</td>
-            <td>${registro.comentario_opcional || "-"} <br> ${botaoAtualizarComentario}</td>
+            <td>${
+              registro.comentario_opcional || "-"
+            } <br> ${botaoAtualizarComentario}</td>
             <td>${imagemCell}</td>
             <td>${dataPassadaText}</td>
             <td>${botaoApagar}</td>
         `;
 
-        tabelaBody.appendChild(row);
-    });
+    tabelaBody.appendChild(row);
+  });
 
-    // Adicionar eventos de clique para os botões
-
-    // Atualizar Comentário
-    document.querySelectorAll(".btn-atualizar-comentario").forEach(botao => {
-        botao.addEventListener("click", (event) => {
-            const id = event.target.getAttribute("data-id");
-            abrirDialogoAtualizarCampo(id, "comentario_opcional"); // Abre diálogo para atualizar o comentário
-        });
+  document.querySelectorAll(".btn-atualizar-comentario").forEach((botao) => {
+    botao.addEventListener("click", (event) => {
+      const id = event.target.getAttribute("data-id");
+      abrirDialogoAtualizarCampo(id, "comentario_opcional");
     });
+  });
 
-    // Atualizar Tipo de Ponto
-    document.querySelectorAll(".btn-atualizar-tipo").forEach(botao => {
-        botao.addEventListener("click", (event) => {
-            const id = event.target.getAttribute("data-id");
-            abrirDialogoAtualizarCampo(id, "tipo"); // Abre diálogo para atualizar o tipo de ponto
-        });
+  document.querySelectorAll(".btn-atualizar-tipo").forEach((botao) => {
+    botao.addEventListener("click", (event) => {
+      const id = event.target.getAttribute("data-id");
+      abrirDialogoAtualizarCampo(id, "tipo");
     });
+  });
 
-    // Atualizar Data
-    document.querySelectorAll(".btn-atualizar-data").forEach(botao => {
-        botao.addEventListener("click", (event) => {
-            const id = event.target.getAttribute("data-id");
-            abrirDialogoAtualizarCampo(id, "data"); // Abre diálogo para atualizar a data
-        });
+  document.querySelectorAll(".btn-atualizar-data").forEach((botao) => {
+    botao.addEventListener("click", (event) => {
+      const id = event.target.getAttribute("data-id");
+      abrirDialogoAtualizarCampo(id, "data");
     });
+  });
 
-    // Atualizar Hora
-    document.querySelectorAll(".btn-atualizar-hora").forEach(botao => {
-        botao.addEventListener("click", (event) => {
-            const id = event.target.getAttribute("data-id");
-            abrirDialogoAtualizarCampo(id, "hora"); // Abre diálogo para atualizar a hora
-        });
+  document.querySelectorAll(".btn-atualizar-hora").forEach((botao) => {
+    botao.addEventListener("click", (event) => {
+      const id = event.target.getAttribute("data-id");
+      abrirDialogoAtualizarCampo(id, "hora");
     });
+  });
 
-    // Adicionar eventos de clique para o botão de apagar (apenas alerta)
-    document.querySelectorAll(".btn-apagar").forEach(botao => {
-        botao.addEventListener("click", () => {
-            alert("Os registros não podem ser apagados.");
-        });
+  document.querySelectorAll(".btn-apagar").forEach((botao) => {
+    botao.addEventListener("click", () => {
+      alert("Os registros não podem ser apagados.");
     });
+  });
 }
 
-// Função para abrir o diálogo de atualização com input de data/hora
-// Função para abrir o diálogo de atualização com input de data/hora ou tipo
-// Função para abrir o diálogo de atualização com input de data/hora ou tipo
 function abrirDialogoAtualizarCampo(id, campo) {
-    const registros = JSON.parse(localStorage.getItem("register")) || [];
-    const registro = registros.find(r => r.id == id);
+  const registros = JSON.parse(localStorage.getItem("register")) || [];
+  const registro = registros.find((r) => r.id == id);
 
-    if (registro) {
-        const dialogAtualizacao = document.getElementById("dialog-atualizar");
-        dialogAtualizacao.showModal();
+  if (registro) {
+    const dialogAtualizacao = document.getElementById("dialog-atualizar");
+    dialogAtualizacao.showModal();
 
-        const novoValorInput = document.getElementById("novo-valor");
+    const novoValorInput = document.getElementById("novo-valor");
 
-        if (campo === "data") {
-            novoValorInput.setAttribute("type", "date");
-            novoValorInput.value = new Date(registro.data.split("/").reverse().join("-")).toISOString().split('T')[0];
-        } else if (campo === "hora") {
-            novoValorInput.setAttribute("type", "time");
-            novoValorInput.value = registro.hora;
-        } else if (campo === "tipo") {
-            // Cria o select para os tipos de ponto
-            const select = document.createElement("select");
-            select.id = "novo-tipo";
+    if (campo === "data") {
+      novoValorInput.setAttribute("type", "date");
+      novoValorInput.value = new Date(
+        registro.data.split("/").reverse().join("-")
+      )
+        .toISOString()
+        .split("T")[0];
+    } else if (campo === "hora") {
+      novoValorInput.setAttribute("type", "time");
+      novoValorInput.value = registro.hora;
+    } else if (campo === "tipo") {
+      // Cria o select para os tipos de ponto
+      const select = document.createElement("select");
+      select.id = "novo-tipo";
 
-            const tipos = ["Entrada", "Intervalo", "Volta Intervalo", "Saída", "Falta"];
-            tipos.forEach(tipo => {
-                const option = document.createElement("option");
-                option.value = tipo.toLowerCase();
-                option.text = tipo;
-                if (registro.tipo === tipo.toLowerCase()) {
-                    option.selected = true;
-                }
-                select.appendChild(option);
-            });
-
-            // Substitui o input atual pelo select
-            const inputContainer = novoValorInput.parentNode;
-            inputContainer.replaceChild(select, novoValorInput);
-        } else {
-            novoValorInput.setAttribute("type", "text");
-            novoValorInput.value = registro[campo];
+      const tipos = [
+        "Entrada",
+        "Intervalo",
+        "Volta Intervalo",
+        "Saída",
+        "Falta",
+      ];
+      tipos.forEach((tipo) => {
+        const option = document.createElement("option");
+        option.value = tipo.toLowerCase();
+        option.text = tipo;
+        if (registro.tipo === tipo.toLowerCase()) {
+          option.selected = true;
         }
+        select.appendChild(option);
+      });
 
-        // Ação de confirmação
-        document.getElementById("btn-confirmar-atualizacao").onclick = () => {
-            let novoValor;
-            if (campo === "tipo") {
-                const selectTipo = document.getElementById("novo-tipo");
-                novoValor = selectTipo.value;
-            } else {
-                novoValor = novoValorInput.value;
-            }
-
-            if (novoValor && confirm("Deseja atualizar o valor?")) {
-                atualizarCampoRegistro(id, campo, novoValor);
-                dialogAtualizacao.close();
-            }
-        };
-
-        document.getElementById("btn-cancelar-atualizacao").onclick = () => {
-            dialogAtualizacao.close();
-        };
+      const inputContainer = novoValorInput.parentNode;
+      inputContainer.replaceChild(select, novoValorInput);
+    } else {
+      novoValorInput.setAttribute("type", "text");
+      novoValorInput.value = registro[campo];
     }
+
+    document.getElementById("btn-confirmar-atualizacao").onclick = () => {
+      let novoValor;
+      if (campo === "tipo") {
+        const selectTipo = document.getElementById("novo-tipo");
+        novoValor = selectTipo.value;
+      } else {
+        novoValor = novoValorInput.value;
+      }
+
+      if (novoValor && confirm("Deseja atualizar o valor?")) {
+        atualizarCampoRegistro(id, campo, novoValor);
+        dialogAtualizacao.close();
+      }
+    };
+
+    document.getElementById("btn-cancelar-atualizacao").onclick = () => {
+      dialogAtualizacao.close();
+    };
+  }
 }
 
-// Função para atualizar o registro no LocalStorage
 function atualizarCampoRegistro(id, campo, novoValor) {
-    const registros = JSON.parse(localStorage.getItem("register")) || [];
-    const registro = registros.find(r => r.id == id);
+  const registros = JSON.parse(localStorage.getItem("register")) || [];
+  const registro = registros.find((r) => r.id == id);
 
-    if (registro) {
-        // Se o campo a ser atualizado é o tipo, e o tipo anterior era "falta" e está sendo alterado para outro tipo, apaga a justificativa
-        if (campo === "tipo" && registro.tipo === "falta" && novoValor !== "falta") {
-            registro.justificativa = null; // Remove a justificativa
-        }
-
-        registro[campo] = novoValor; // Atualiza o campo com o novo valor
-
-        // Atualiza o LocalStorage com o novo valor
-        localStorage.setItem("register", JSON.stringify(registros));
-        alert("Registro atualizado com sucesso!");
-
-        // Re-renderiza a lista de registros
-        renderList(registros);
+  if (registro) {
+    if (
+      campo === "tipo" &&
+      registro.tipo === "falta" &&
+      novoValor !== "falta"
+    ) {
+      registro.justificativa = null;
     }
+
+    registro[campo] = novoValor;
+
+    localStorage.setItem("register", JSON.stringify(registros));
+    alert("Registro atualizado com sucesso!");
+
+    renderList(registros);
+  }
 }
 
-// Função para aplicar o filtro de datas
 function aplicarFiltro() {
-    const dataInicial = document.getElementById("data-inicial").value;
-    const dataFinal = document.getElementById("data-final").value;
+  const dataInicial = document.getElementById("data-inicial").value;
+  const dataFinal = document.getElementById("data-final").value;
 
-    if (!dataInicial || !dataFinal) {
-        alert("Por favor, selecione um período válido para o filtro.");
-        return;
-    }
+  if (!dataInicial || !dataFinal) {
+    alert("Por favor, selecione um período válido para o filtro.");
+    return;
+  }
 
-    const registros = JSON.parse(localStorage.getItem("register")) || [];
+  const registros = JSON.parse(localStorage.getItem("register")) || [];
 
-    const registrosFiltrados = registros.filter(registro => {
-        const dataRegistro = new Date(registro.data.split('/').reverse().join('-')); // Converte a data para YYYY-MM-DD
-        const dataInicio = new Date(dataInicial);
-        const dataFim = new Date(dataFinal);
+  const registrosFiltrados = registros.filter((registro) => {
+    const dataRegistro = new Date(registro.data.split("/").reverse().join("-"));
+    const dataInicio = new Date(dataInicial);
+    const dataFim = new Date(dataFinal);
 
-        return dataRegistro >= dataInicio && dataRegistro <= dataFim;
-    });
+    return dataRegistro >= dataInicio && dataRegistro <= dataFim;
+  });
 
-    renderList(registrosFiltrados);
+  renderList(registrosFiltrados);
 }
 
-// Adiciona o evento de clique no botão de filtro
 document.getElementById("btn-filtrar").addEventListener("click", aplicarFiltro);
 
-// Renderiza todos os registros ao carregar a página
 renderList(JSON.parse(localStorage.getItem("register")) || []);
